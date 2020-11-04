b2f37610fd9630e64e170f786355fea9a6600bcc
sali-k <saki.kimura78@gmail.com>
2020-11-04 20:29:35 +0900

    更新処理を追加

--- src/model/TodoListModel.js
+++ src/model/TodoListModel.js
@@ -26,4 +26,13 @@ export class TodoListModel extends EventEmitter {
     this.items.push(todoItem);
     this.emitChange();
   }
+
+  updateTodo({ id, completed }) {
+    const todoItem = this.items.find((todo) => todo.id === id);
+    if (!todoItem) {
+      return;
+    }
+    todoItem.completed = completed;
+    this.emitChange();
+  }
 }
