c1c5d0c1f9dec0e8aa31ef8c343d5ff9e7ae9821
sali-k <saki.kimura78@gmail.com>
2020-11-04 20:47:54 +0900

    TodoItemModel.js修正

--- src/model/TodoItemModel.js
+++ src/model/TodoItemModel.js
@@ -1,9 +1,13 @@
 let todoIdx = 0;
 
 export class TodoItemModel {
-  constructor({ title, completed }) {
-    this.id = todoIdx++;
-    this.title = title;
-    this.completed = completed;
-  }
+    constructor({ title, completed }) {
+        this.id = todoIdx++;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    isEmptyTitle() {
+        return this.title.length === 0;
+    }
 }
