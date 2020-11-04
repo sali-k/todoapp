10e5ee30d25efb66f62f9f9348706b3a9900c285
sali-k <saki.kimura78@gmail.com>
2020-11-04 20:39:58 +0900

    TodoListView作成

---
+++ src/view/TodoListView.js
@@ -0,0 +1,18 @@
+import { element } from "./html-util.js";
+import { TodoItemView } from "./TodoItemView.js";
+
+export class TodoListView {
+  createElement(todoItems, { onUpdateTodo, onDeleteTodo }) {
+    const todoListElement = element`<ul />`;
+    // 各TodoItemモデルに対応したHTML要素を作成し、リスト要素へ追加する
+    todoItems.forEach((todoItem) => {
+      const todoItemView = new TodoItemView();
+      const todoItemElement = todoItemView.createElement(todoItem, {
+        onDeleteTodo,
+        onUpdateTodo,
+      });
+      todoListElement.appendChild(todoItemElement);
+    });
+    return todoListElement;
+  }
+}
